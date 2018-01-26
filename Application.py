c5e3454ac1aa8f48424ae036eade6ccb883eafcb
Kicker Tisch <fussball@tisch>
2018-01-26 11:10:33 +0000

    Get neural net working

--- Application.py
+++ Application.py
@@ -5,10 +5,10 @@ import pygame
 import numpy as np
 from multiprocessing import Process, Queue
 import sys, os
-# import tensorflow as tf
+import tensorflow as tf
 import yaml
 
-# from kicker.agents.neural_net_agent import NeuralNetAgent
+from kicker.agents.neural_net_agent import NeuralNetAgent
 from kicker.opcua_motor import MotorController
 
 from kicker import ConsoleView, Helper
@@ -23,23 +23,26 @@ logging.basicConfig(filename='kicker.log', level=logging.DEBUG, format='%(asctim
 logging.info("Fussball ist wie Schach nur ohne Wuerfel")
 
 
-# import keras.backend.tensorflow_backend as KTF
+import keras.backend.tensorflow_backend as KTF
 
-# def get_session(gpu_fraction=0.4):
-#     num_threads = os.environ.get('OMP_NUM_THREADS')
-#     gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
-# 
-#     if num_threads:
-#         return tf.Session(config=tf.ConfigProto(
-#             gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
-#     else:
-#         return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
-# 
-# 
-# def set_keras_gpu_use(percentage):
-#     KTF.set_session(get_session(gpu_fraction = percentage))
-# 
-# set_keras_gpu_use(0.4)
+def get_session(gpu_fraction=0.4):
+    num_threads = os.environ.get('OMP_NUM_THREADS')
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_fraction)
+
+    if num_threads:
+        return tf.Session(config=tf.ConfigProto(
+            gpu_options=gpu_options, intra_op_parallelism_threads=num_threads))
+    else:
+        return tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+
+
+def set_keras_gpu_use(percentage):
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    sess = tf.Session(config=config)
+    KTF.set_session(sess)
+
+set_keras_gpu_use(0.4)
 
 def read_config():
     with open('config.yml', 'r') as f:
@@ -124,8 +127,8 @@ class Application(object):
                 self.submit_inputs()
 
 if __name__ == '__main__':
-    agent = RandomAgent()
+    # agent = RandomAgent()
     # agent = KeyboardAgent()
-    # agent = NeuralNetAgent()
+    agent = NeuralNetAgent()
     program = Application(agent, enable_storage=False)
     program.run()
