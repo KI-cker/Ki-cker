ee3c8f562654d1af68954a354da6ae6020345eb6
Helge <helge.krueger@tngtech.com>
2018-02-19 23:34:23 +0100

    Cleanup OPCUA code

---
+++ kicker/opcua_base.py
@@ -0,0 +1,41 @@
+from opcua import ua
+from opcua import Client
+
+from kicker.opcua_motor import Axis, SubScriptionHandler
+
+
+class OpcuaBase:
+    def __init__(self):
+        self.client = Client("opc.tcp://192.168.42.20:4840")
+
+    def readValue_polling(self, nodeId, namespace):
+        var = self.client.get_node(ua.NodeId(nodeId, namespace))
+        return var.get_value()
+
+    def writeBooleanValue(self, nodeId, namespace, value):
+        var = self.client.get_node(ua.NodeId(nodeId, namespace))
+        var.set_value(ua.Variant(value, ua.VariantType.Boolean))
+
+    def writeFloatValue(self, nodeId, namespace, value):
+        var = self.client.get_node(ua.NodeId(nodeId, namespace))
+        var.set_value(ua.Variant(value, ua.VariantType.Float))
+
+    def disconnect(self):
+        self.client.disconnect()
+
+    def add_subscription(self, AxisNo, AxisType,
+                         MeasurementType):  # AxisNo [1...4], AxisType ["Trans","Rot"], MeasurementType ["Vel","Pos"]
+        if AxisType == "Rot":
+            atype = 0
+        if AxisType == "Trans":
+            atype = 1
+        if MeasurementType == "Pos":
+            mtype = 2
+        if MeasurementType == "Trans":
+            mtype = 3
+        nodeId = Axis[AxisNo - 1][atype][mtype][0]
+        namespace = Axis[AxisNo - 1][atype][mtype][1]
+        var = self.client.get_node(ua.NodeId(nodeId, namespace))
+        handler = SubScriptionHandler()
+        sub = self.client.create_subscription(50, handler)
+        sub.subscribe_data_change(var)
---
+++ kicker/opcua_controller.py
@@ -0,0 +1,89 @@
+import logging
+
+from opcua import ua
+
+from kicker.opcua_base import OpcuaBase
+from kicker.opcua_constants import PosMode_Ack_Rot_Tor, PosMode_Ack_Rot_Verteidiung, PosMode_Ack_Rot_Mittelfeld, \
+    PosMode_Ack_Rot_Sturm, PosMode_Ack_Trans_Tor, PosMode_Ack_Trans_Verteidiung, PosMode_Ack_Trans_Mittelfeld, \
+    PosMode_Ack_Trans_Sturm, Axis
+
+class OpcuaController(OpcuaBase):
+    def __init__(self):
+        super().__init__()
+
+    def resetEmulation(self):
+        var = self.client.get_node(ua.NodeId(resetEmulation[0], resetEmulation[1]))
+        var.set_value(ua.Variant(True, ua.VariantType.Boolean))
+
+    def connect(self):
+        self.client.connect()
+        root = self.client.get_root_node()
+        if (root != ""):
+            logging.info("Connection established")
+            ack_axis1 = self.readValue_polling(PosMode_Ack_Rot_Tor[0], PosMode_Ack_Rot_Tor[1])
+            ack_axis2 = self.readValue_polling(PosMode_Ack_Rot_Verteidiung[0], PosMode_Ack_Rot_Verteidiung[1])
+            ack_axis3 = self.readValue_polling(PosMode_Ack_Rot_Mittelfeld[0], PosMode_Ack_Rot_Mittelfeld[1])
+            ack_axis4 = self.readValue_polling(PosMode_Ack_Rot_Sturm[0], PosMode_Ack_Rot_Sturm[1])
+            ack_axis5 = self.readValue_polling(PosMode_Ack_Trans_Tor[0], PosMode_Ack_Trans_Tor[1])
+            ack_axis6 = self.readValue_polling(PosMode_Ack_Trans_Verteidiung[0], PosMode_Ack_Trans_Verteidiung[1])
+            ack_axis7 = self.readValue_polling(PosMode_Ack_Trans_Mittelfeld[0], PosMode_Ack_Trans_Mittelfeld[1])
+            ack_axis8 = self.readValue_polling(PosMode_Ack_Trans_Sturm[0], PosMode_Ack_Trans_Sturm[1])
+            initDone = ack_axis1 and ack_axis2 and ack_axis3 and ack_axis4 and ack_axis5 and ack_axis6 and ack_axis7 and ack_axis8
+
+            if (initDone != True):
+                 logging.info("axis not ready yet or error")
+
+            if (initDone == True):
+                logging.info("axis ready")
+        else:
+            logging.info("Could not connect to server!")
+
+    def positiveJog_Rot(self, AxisNo):
+        Node = Axis[AxisNo - 1][0][1][0]
+        Namespace = Axis[AxisNo - 1][0][1][1]
+        Value = True
+        self.writeBooleanValue(Node, Namespace, Value)
+        # logging.debug("POSITIVE jog of rotatory axis:", AxisNo, ",NodeID: ns=", Namespace, ";i=", Node, ", Value:", Value)
+
+    def negativeJog_Rot(self, AxisNo):
+        Node = Axis[AxisNo - 1][0][0][0]
+        Namespace = Axis[AxisNo - 1][0][0][1]
+        Value = True
+        self.writeBooleanValue(Node, Namespace, Value)
+        # logging.debug("NEGATIVE jog of rotatory axis:", AxisNo, ",NodeID: ns=", Namespace, ";i=", Node, ", Value:", Value)
+
+    def stopJog_Rot(self, AxisNo):
+        Node1 = Axis[AxisNo - 1][0][0][0]
+        Namespace1 = Axis[AxisNo - 1][0][0][1]
+        Value1 = False
+        Node2 = Axis[AxisNo - 1][0][1][0]
+        Namespace2 = Axis[AxisNo - 1][0][1][1]
+        Value2 = False
+        self.writeBooleanValue(Node1, Namespace1, Value1)
+        self.writeBooleanValue(Node2, Namespace2, Value2)
+        # logging.debug("STOP jog of rotatory axis:", AxisNo)
+
+    def positiveJog_Trans(self, AxisNo):
+        Node = Axis[AxisNo - 1][1][1][0]
+        Namespace = Axis[AxisNo - 1][1][1][1]
+        Value = True
+        self.writeBooleanValue(Node, Namespace, Value)
+        # logging.debug("POSITIVE jog of translatory axis:", AxisNo, ",NodeID: ns=", Namespace, ";i=", Node, ", Value:", Value)
+
+    def negativeJog_Trans(self, AxisNo):
+        Node = Axis[AxisNo - 1][1][0][0]
+        Namespace = Axis[AxisNo - 1][1][0][1]
+        Value = True
+        self.writeBooleanValue(Node, Namespace, Value)
+        # logging.debug("NEGATIVE jog of translatory axis:", AxisNo, ",NodeID: ns=", Namespace, ";i=", Node, ", Value:", Value)
+
+    def stopJog_Trans(self, AxisNo):
+        Node1 = Axis[AxisNo - 1][1][0][0]
+        Namespace1 = Axis[AxisNo - 1][1][0][1]
+        Value1 = False
+        Node2 = Axis[AxisNo - 1][1][1][0]
+        Namespace2 = Axis[AxisNo - 1][1][1][1]
+        Value2 = False
+        self.writeBooleanValue(Node1, Namespace1, Value1)
+        self.writeBooleanValue(Node2, Namespace2, Value2)
+        # logging.debug("STOP jog of translatory axis:", AxisNo)
