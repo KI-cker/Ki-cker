f46e11aec68f4ab878bc920fec40e503812b4f43
Helge <helge.krueger@tngtech.com>
2018-03-17 17:06:02 +0100

    Add routine to retrieve all games

--- kicker/train/parser.py
+++ kicker/train/parser.py
@@ -32,4 +32,25 @@ class Parser:
         actions = [a for a in data['actions']]
         scores = [s for s in data['ball_scores']]
 
-        return table_frames, positions, actions, scores
+        return table_frames, positions, actions, scoress
+
+    def get_train_game_data(self, game):
+        data = self.file[game]
+        table_frames = [self.decode_image(i)[:,:,1] for i in data['table_frames_encoded']]
+        positions = [p for p in data['ball_pos']]
+        actions = [a for a in data['actions']]
+        scores = [s for s in data['scores']]
+        goals_received = [s for s in data['goals_received']]
+        good_indices = [i for i in data['good_indices']]
+
+        length = len(table_frames)
+
+        observations = [np.swapaxes(np.swapaxes(table_frames[k:k+6], 0, 2), 0, 1) for k in range(0, length - 7)]
+
+        return [{
+            'action': actions[k + 5],
+            'score': scores[k + 5],
+            'images': observations[k],
+            'terminal': k + 5 in goals_received
+        } for k in range(0, length - 7) if k + 5 in good_indices]
+
