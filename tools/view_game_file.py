4081e9663ba62763a6b4580a8ac1d6f907f85590
Helge <helge.krueger@tngtech.com>
2018-03-18 18:31:42 +0100

    Add socket stuff

--- tools/view_game_file.py
+++ tools/view_game_file.py
@@ -1,5 +1,7 @@
 import cv2
 import numpy as np
+import time
+import json
 
 from kicker.train import DataProvider, Parser
 from kicker.neural_net import NeuralNet
@@ -12,22 +14,28 @@ nn = NeuralNet(23, (320, 480, 5), filename='/home/helge/tng/kicker/Ki-cker/model
 
 fig = Figure(wait_for_button_press=False, show_images=True)
 
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
 def show_prediction(frames, position):
     observation = np.concatenate([(f[:,:,1]).reshape((320, 480, 1)) for f in frames], axis=2)
     prediction = nn.predict_single(observation).reshape(8, 3)
 
-    print(np.argmax(prediction, axis=1) - np.ones(8), position)
+    print(np.argmax(prediction, axis=1) - np.ones(8), position, np.min(prediction))
 
-    ball_frame = frames[4].copy()
-    cv2.circle(ball_frame, tuple(position + 9), 9, (255, 0, 0))
-
-    fig.plot(frames[2], frames[3], ball_frame, prediction)
+    # ball_frame = frames[4].copy()
+    # cv2.circle(ball_frame, tuple(position + 9), 9, (255, 0, 0))
+    #
+    # fig.plot(frames[2], frames[3], ball_frame, prediction)
+    sock.sendto(json.dumps(np.max(prediction, axis=1).tolist()).encode(), ('192.168.0.49', 1881))
+    # time.sleep(1.0/10)
 
 # parser = Parser(filename='train/Fabian_20180209_095009.h5')
 # parser = Parser(filename='train/Fabian_20180209_133730.h5')
 # parser = Parser(filename='train/Fabian_20180308_080924.h5')
-# parser = Parser(filename='train/Fabian_20180314_133913.h5')
-parser = Parser(filename='train/Fabian_20180314_091655.h5')
+parser = Parser(filename='train/Fabian_20180314_133913.h5')
+# parser = Parser(filename='train/Fabian_20180314_091655.h5')
 
 for game_name in parser.file:
     table_frames, positions, actions, scores = parser.get_game_data(game_name)
