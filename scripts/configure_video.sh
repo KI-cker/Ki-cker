21bf4de8a3e94280d67a116f9c596721be522d28
Helge <helge.krueger@tngtech.com>
2018-03-20 22:05:04 +0100

    Webcam settings

--- scripts/configure_video.sh
+++ scripts/configure_video.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 v4l2-ctl -d /dev/video1 --set-ctrl focus_auto=0
+v4l2-ctl -d /dev/video1 --set-ctrl power_line_frequency=1
+v4l2-ctl -d /dev/video1 --set-ctrl backlight_compensation=0
+v4l2-ctl -d /dev/video1 --set-ctrl exposure_auto_priority=1
